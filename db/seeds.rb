676563e4be2cd0e5e590c7cadfd0f5a94cc4001e
quentin natoly <quentin.natoly@gmail.com>
2020-05-13 17:02:48 +0200

    Exercice 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Amsterdam appart',
+  address: 'i don\'t know the address',
+  description: 'Hotel-like room in Amsterdam Centrum - canal view',
+  price_per_night: 100,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Paris appart',
+  address: 'a strange address',
+  description: 'Jolie studio avec jardin proche paris , la defense',
+  price_per_night: 150,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'Las vegas',
+  address: 'i forgot the address',
+  description: '1H11 Penthouse Floor Best Bellagio Fountain View',
+  price_per_night: 500,
+  number_of_guests: 6
+)
